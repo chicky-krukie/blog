4131881942092bf442cd52c8d5fb82e31a0bc0d4
Ngebu <kimfrancis.villacer@tup.edu.ph>
2023-03-23 12:07:32 -0700

    search bar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -62,9 +62,11 @@ class PostController extends Controller
         return redirect('blog')->with('flash_message', 'Post Deleted Successfully');
     }
     
-    public function search(Request $request)
+    public function search()
     {
-        $get_blog = $request->search_name;
-        
+        $search_text = $_GET['title'];
+        $blog = Post::where('title', 'LIKE', '%'.$search_text.'%')->get();
+
+        return view('blog.search', compact('blog'));
     }
 }
